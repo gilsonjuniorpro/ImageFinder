40e9c8eb3f318b382542dd5a1bf9add1b2cd48c1
gilsonjuniorpro <gilsonjuniorpro@gmail.com>
2022-05-22 05:55:00 -0400

    setup hilt

---
+++ app/src/main/java/imagefinder/ca/di/AppModule.kt
@@ -0,0 +1,31 @@
+package imagefinder.ca.di
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ApplicationComponent
+import imagefinder.ca.data.remote.UnsplashApi
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(ApplicationComponent::class)
+object AppModule {
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl(UnsplashApi.BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideUnsplashApi(retrofit: Retrofit): UnsplashApi {
+        return retrofit.create(UnsplashApi::class.java)
+    }
+
+}
