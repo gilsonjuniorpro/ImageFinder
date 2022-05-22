e31c7d06cc6d33dc8e18a63c4b552f576147bf29
gilsonjuniorpro <gilsonjuniorpro@gmail.com>
2022-05-22 04:51:25 -0400

    create api

---
+++ app/src/main/java/imagefinder/ca/data/local/UnsplashRepository.kt
@@ -0,0 +1,25 @@
+package imagefinder.ca.data.local
+
+import androidx.paging.Pager
+import androidx.paging.PagingConfig
+import androidx.paging.liveData
+import imagefinder.ca.data.remote.UnsplashApi
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+class UnsplashRepository @Inject constructor(
+    private val unsplashApi: UnsplashApi
+) {
+
+    fun getSearchResults(query: String) =
+        Pager(
+            config = PagingConfig(
+                pageSize = 20,
+                maxSize = 100,
+                enablePlaceholders = false
+            ),
+            pagingSourceFactory = { UnsplashPagingSource(unsplashApi, query) }
+        ).liveData
+
+}
